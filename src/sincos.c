d40efbebb997c221aaceb7197266141cc8d70804
Louis Strous <louis.strous@gmail.com>
2014-08-19 09:30:33 +0200

    Forgot to commit sincos.c

---
+++ src/sincos.c
@@ -0,0 +1,38 @@
+/* This is file sincos.c.
+
+Copyright 2014 Louis Strous
+
+This file is part of LUX.
+
+LUX is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+LUX is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with LUX.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#define _GNU_SOURCE             /* for sincos */
+
+#include <math.h>               /* for sincos */
+#include "action.h"             /* for BIND */
+
+#if !HAVE_SINCOS
+void sincos(double x, double *sinx, double *cosx)
+{
+  if (sinx)
+    *sinx = sin(x);
+  if (cosx)
+    *cosx = cos(x);
+}
+#endif
+BIND(sincos, v_ddpdp_iDaoDqDq_012, s, SINCOS, 3, 3, NULL);

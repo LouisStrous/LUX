cd5356755aa181c2c9e24377211973d1eeff68d3
Louis Strous <louis@quae.nl>
2023-12-12 21:10:39 +0100

    Make the Gsl_root_fsolver status available
    and document the declarations.

--- src/gslpp_roots.cc
+++ src/gslpp_roots.cc
@@ -20,9 +20,14 @@ Gsl_root_fsolver::Gsl_root_fsolver(const gsl_root_fsolver_type* type,
   : d_solver(gsl_root_fsolver_alloc(type)),
     d_functor(f)
 {
-  d_gslf.function = &unwrap_for_gsl_function;
-  d_gslf.params = (void*) &d_functor;
-  gsl_root_fsolver_set(d_solver, &d_gslf, x_lo, x_hi);
+  if (!d_solver)
+    d_status = GSL_ENOMEM;
+  else
+  {
+    d_gslf.function = &unwrap_for_gsl_function;
+    d_gslf.params = (void*) &d_functor;
+    d_status = gsl_root_fsolver_set(d_solver, &d_gslf, x_lo, x_hi);
+  }
 }
 
 Gsl_root_fsolver::~Gsl_root_fsolver()
@@ -30,22 +35,30 @@ Gsl_root_fsolver::~Gsl_root_fsolver()
   gsl_root_fsolver_free(d_solver);
 }
 
+int
+Gsl_root_fsolver::get_status() const
+{
+  return d_status;
+}
+
 double
 Gsl_root_fsolver::get_root()
 {
-  int status;
+  if (d_status)
+    return std::nan("");
+
   size_t iterations = 0;
 
   do
   {
     ++iterations;
-    status = gsl_root_fsolver_iterate(d_solver);
-    if (!status) {              // no problem during iteration
+    d_status = gsl_root_fsolver_iterate(d_solver);
+    if (!d_status) {            // no problem during iteration
       double x_lo = gsl_root_fsolver_x_lower(d_solver);
       double x_hi = gsl_root_fsolver_x_upper(d_solver);
-      status = gsl_root_test_interval(x_lo, x_hi, 0, 0.001);
+      d_status = gsl_root_test_interval(x_lo, x_hi, 0, 10*DBL_EPSILON);
     }
-  } while (status == GSL_CONTINUE && iterations < 100);
+  } while (d_status == GSL_CONTINUE && iterations < 100);
 
   return gsl_root_fsolver_root(d_solver);
 }
--- src/gslpp_roots.hh
+++ src/gslpp_roots.hh
@@ -8,19 +8,42 @@
 
 double unwrap_for_gsl_function(double x, void* params);
 
+/// A C++ wrapper around gsl_root_fsolver.
 class Gsl_root_fsolver
 {
 public:
   // constructor, destructor
 
+  /// Constructor.
+  ///
+  /// \param type is the type of the GSL root solver.
+  ///
+  /// \param f is the function-like thing (functor) to find the root for.
+  ///
+  /// \param x_lo is the lower bound of the range of the independent variable to
+  /// search.
+  ///
+  /// \param x_hi is the upper bound of the range of the independent variable to
+  /// search.
   Gsl_root_fsolver(const gsl_root_fsolver_type* type,
                    std::function<double(double)> f, double x_lo,
                    double x_hi);
 
+  /// Destructor.
   virtual ~Gsl_root_fsolver();
+
+  // const method
+
+  /// Get the status of the GSL root solver.  0 means OK, non-0 means a problem.
+  ///
+  /// \returns the status.
+  int get_status() const;
 
   // non-const methods
 
+  /// Get the estimate for the root, the value of the independent variable for
+  /// which the function returns the value 0.  If no root could be found within
+  /// the specified range then a Not-a-Number value is returned.
   double get_root();
 
 private:
